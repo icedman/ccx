2ee0796aaea843a3db9f2720964ea8aff59f5fae
Marvin <m4rvin2005@gmail.com>
2013-06-01 10:26:20 +0800

    renamed ScrollLayer to TouchLayer

---
+++ Cocos 2d Extensions/CCXTouchLayer.h
@@ -0,0 +1,36 @@
+//
+//  CCXScrollLayer.h
+//  Cocos2d Extensions
+//
+//  Created by Marvin Sanchez on 5/1/13.
+//
+//
+
+#import "CCLayer.h"
+
+@interface CCXTouchLayer : CCLayer
+
+@property CGPoint   scrollPosition;
+@property CGRect    scrollArea;
+@property Float32   scrollFriction;
+@property CGPoint   pressPosition;
+@property CGPoint   releasePosition;
+@property CGPoint   dragPosition;
+@property Float32   zoom;
+@property bool      consumeTouches;
+@property bool      hasTouches;
+@property bool      enabled;
+
+- (void) onTouchBegan;
+- (void) onTouchUpdate;
+- (void) onTouchEnded;
+- (void) onScrollBegan;
+- (void) onScrollUpdate;
+- (void) onScrollEnded;
+- (void) onDoubleTap;
+
+- (Boolean) hasScrollVelocity;
+- (Boolean) isScrolling;
+- (void) scaleVelocity:(Float32)f;
+
+@end
